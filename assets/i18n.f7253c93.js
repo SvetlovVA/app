1cd7f3c3559485528ad5243c78ee3e107c0a80b2
svetlov.v.a <svetlov.v.a@effective-group.ru>
2024-03-17 23:14:42 +0300

    test-8

--- assets/i18n.f7253c93.js
+++ assets/i18n.f7253c93.js
@@ -1,4 +1,4 @@
-import{d as ot,h as jt,e as In,i as On,o as bn,a as hn,c as be,w as Ze,F as Bt,g as Jt,b as Cn,r as An,s as Sn,f as Pn,T as yn,j as Rn}from"./index.481f1397.js";/*!
+import{d as ot,h as jt,e as In,i as On,o as bn,a as hn,c as be,w as Ze,F as Bt,g as Jt,b as Cn,r as An,s as Sn,f as Pn,T as yn,j as Rn}from"./index.cf64593d.js";/*!
   * shared v9.9.1
   * (c) 2024 kazuya kawaguchi
   * Released under the MIT License.
